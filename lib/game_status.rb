9d5d83b426d531c73feee93073b6d66679b4e510
Sam B <s77broz@aol.com>
2018-09-07 01:39:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,7 +20,7 @@ def won?(board)
     val1 = board[combo[0]]
     val2 = board[combo[1]]
     val3 = board[combo[2]]
-    if val1 == val2 && val2 == val3
+    if position_taken?(board, combo[0]) && val1 == val2 && val2 == val3
       return combo
     end
   end
